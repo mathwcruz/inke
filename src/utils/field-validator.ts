26d4ef5785f7d0edb79837c4562134c609913603
Matheus Cruz <matheuswachcruz@gmail.com>
2022-06-23 22:19:25 -0300

    feat: create email field validator

---
+++ src/utils/field-validator.ts
@@ -0,0 +1,10 @@
+const Regex = {
+  email:
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+};
+
+export const validateEmailField = (email: string) => {
+  if (!email) return null;
+
+  return Regex.email.test(email);
+};
